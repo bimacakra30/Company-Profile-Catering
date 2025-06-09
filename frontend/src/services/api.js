e0f120f02690c97a313b2ee6195693c2ffb6405e
bimacakra <biimackra@gmail.com>
2025-06-10 06:56:07 +0700

    Test

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://172.16.10.254/api/',
+  baseURL: 'http://127.0.0.1/api/',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
   },
 });
 
-export const BASE_IMAGE_URL = 'http:/172.16.10.254/storage/';
+export const BASE_IMAGE_URL = 'http://127.0.0.1/storage/';
 
 export const getCategories = () => api.get('/categories');
 export const getGalleries = () => api.get('/galleries');
